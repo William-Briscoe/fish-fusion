19da959c9a9cfff1122511f10f462bbfbc6868c8
William Briscoe <will.m.briscoe@gmail.com>
2023-04-13 09:58:28 -0500

    initial fish fusion comments

---
+++ fishMonger.js
@@ -0,0 +1,17 @@
+// import boat inventory function
+//invoke boatInventory, which generates todaysCatch array
+
+//create a function called fishmonger with a for of loop that iterates through todaysCatch
+    //declare empty array called mongerInventory []
+    //create ifand statement that checks if fish price is <= 7.50  && fish quantity >= 10
+    //then .push array method to add objects that meet the above requirements to the empty mongerInventory array
+    //return mongerInventory array
+//declare a number variable called chefBudget that represents the highest price the chef will pay for a fish that day
+
+//create a function called chefsOrder with a for of loop that iterates through mongerInventory array
+    //declare empty array called chefsInventory []
+    //create if statement that checks if fish price is <= chefBudget
+    //then todaysCatch.amount = math.floor(mongerInventory.amount / 2 )
+    //use .push array method to add objects that meet the above requirements to the empty chefsInventory array
+    //return chefsInventory array
+//export fishmonger and chefsOrder functions to restaurant.js
---
+++ restaurant.js
@@ -0,0 +1,6 @@
+//import fishmonger function from fishMonger.js
+//invoke fishmonger function and call the modified array chefsInventory
+//declare a function fishMenu
+    //argument will be the chefBudget (variable declared in fishMonger module) and
+    //passed to fishInspector function within fishMonger.js
+//create function that looks into boatInventory
